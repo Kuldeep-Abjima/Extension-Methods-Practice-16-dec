5732282e240e202e89697f5a9fdf48321486453e
Kuldeep-Abjima <119651692+Kuldeep-Abjima@users.noreply.github.com>
2022-12-16 18:23:50 +0530

    Add project files.

---
+++ ExtensionMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using MYarrayLIb;
+namespace ExtensionMethod
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("enter the Size of an array : ");
+            int size = int.Parse(Console.ReadLine());
+            int[] a = new int[size];
+            Console.WriteLine("enter the elements");
+            for(int i = 0; i<a.Length; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"sum of this array is : {a.SUM()}");
+            Console.WriteLine($"Max of this array is : {a.MAX()}");
+            Console.WriteLine($"Avg of this array is : {a.AVG()}");
+            Console.WriteLine($"Min of this array is : {a.MIN()}");
+        }
+    }
+}
---
+++ MyLib/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace MYarrayLIb
+{
+    public static class Count
+    {
+
+
+        public static double AVG(this int[] num)
+        {
+            double avg = 0, count = 0;
+            double result = 0;
+            foreach (double i in num)
+            {
+                count++;
+                avg += i;
+            }
+            result = avg / count;
+            return result;
+
+        }
+        public static int SUM(this int[] num)
+        {
+            int sum = 0;
+            for (int i = 0; i < num.Length; i++)
+            {
+                sum += num[i];
+            }
+            return sum;
+        }
+        public static int MAX(this int[] num)
+        {
+            int max = num[0];
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (max < num[i])
+                {
+                    max = num[i];
+                }
+
+            }
+            return max;
+        }
+        public static int MIN(this int[] num)
+        {
+            int min = num[0];
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (min > num[i])
+                {
+                    min = num[i];
+
+                }
+
+            }
+            return min;
+        }
+    }
+
+}
